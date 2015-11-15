e02b9dd823998725854b83e3d31d484495dc82d5
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-11-15 17:03:53 -0600

    Movie service [Broken]

--- MovieAPI/Services/Models/MovieDTO.cs
+++ MovieAPI/Services/Models/MovieDTO.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MovieAPI.Domain.Models {
-    public class Movie {
+namespace MovieAPI.Services.Models {
+    public class MovieDTO {
 
         public int Id { get; set; }
         public string Title { get; set; }
---
+++ MovieAPI/Services/MovieService.cs
@@ -0,0 +1,39 @@
+﻿using CoderCamps.Data.Repository;
+using MovieAPI.Domain.Models;
+using MovieAPI.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieAPI.Services {
+    public class MovieService {
+
+        private IRepository _repo;
+
+        public MovieService(IRepository repo) {
+            _repo = repo;
+        }
+
+        public MovieDTO Map(Movie movie) {
+            MovieDTO dto = new MovieDTO();
+            dto.Id = movie.Id;
+            dto.Title = movie.Title;
+            dto.ReleaseDate = movie.ReleaseDate;
+
+            return dto;
+        }
+
+        public Movie Map(MovieDTO dto) {
+            Movie dbMovie = null;
+            if ((dbMovie = FindInternal(dto.Id)) == null) {
+                dbMovie = new Movie();
+            }
+
+        }
+
+        private Movie FindInternal(int id) {
+            return _repo.Find<Movie>(id);
+        }
+    }
+}
