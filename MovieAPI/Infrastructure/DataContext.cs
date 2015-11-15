16e97f49cd805a6903afa4c6e5392862751b7c65
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-11-15 16:22:43 -0600

    Empty project

---
+++ MovieAPI/Infrastructure/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieAPI.Infrastructure {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) {
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+    }
+}
